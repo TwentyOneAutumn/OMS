dc1f119a6a4379265911d57afd1bfd27f4ecc103
第二十一个秋天 <920614827@qq.com>
2023-03-18 10:40:37 +0800

    111

--- src/main/java/com/oms/controller/LoginController.java
+++ src/main/java/com/oms/controller/LoginController.java
@@ -1,15 +1,11 @@
 package com.oms.controller;
 
-import com.oms.service.ILoginService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
 
     @GetMapping("/toLogin")
