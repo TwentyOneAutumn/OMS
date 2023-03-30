8f4368c3cdd98f304147e47f8450ffb009ac506e
第二十一个秋天 <920614827@qq.com>
2023-03-30 12:17:07 +0800

    111

---
+++ src/main/java/com/oms/utils/CookieUtils.java
@@ -0,0 +1,29 @@
+package com.oms.utils;
+
+import cn.hutool.core.util.ArrayUtil;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CookieUtils {
+
+    public static String get(HttpServletRequest request,String key){
+        Map<String, String> map = new HashMap<>();
+        Cookie[] cookies = request.getCookies();
+        if(ArrayUtil.isNotEmpty(cookies)){
+            return null;
+        }else {
+            for (Cookie cookie : cookies) {
+                map.put(cookie.getName(),cookie.getValue());
+            }
+            return map.get(key);
+        }
+    }
+
+    public static void set(HttpServletResponse response,Map<String, String> map){
+        map.forEach((key,value) -> response.addCookie(new Cookie(key,value)));
+    }
+}
